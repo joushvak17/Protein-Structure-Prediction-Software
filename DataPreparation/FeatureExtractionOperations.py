15ebd02c304af1fed07a06a7252b8b480a2337cd
joushvak17 <kamble.joushva@gmail.com>
2024-04-19 20:11:43 -0400

    Implemented some of the feature extractions for the aligned data in the FeatureExtraction.ipynb file.

--- DataPreparation/FeatureExtractionOperations.py
+++ DataPreparation/FeatureExtractionOperations.py
@@ -1,11 +1,11 @@
 from Bio.SeqUtils import ProtParamData
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
+import subprocess
 
 
 def calculate_a_acid_composition(sequence):
     protein_analysis = ProteinAnalysis(sequence)
     aac_dict = protein_analysis.get_amino_acids_percent()
-
     return aac_dict
 
 
@@ -13,24 +13,20 @@ def calculate_hydrophobicity(sequence):
     protein_analysis = ProteinAnalysis(sequence)
     hydrophobicity_values = protein_analysis.protein_scale(ProtParamData.kd,
                                                            window=9)
-
     return hydrophobicity_values
 
 def calculate_polarity(sequence, ph):
     protein_analysis = ProteinAnalysis(sequence)
     net_charge = protein_analysis.charge_at_pH(ph)
-
     return net_charge
 
 def calculate_mw(sequence):
     protein_analysis = ProteinAnalysis(sequence)
     molecular_weight = protein_analysis.molecular_weight()
-
     return molecular_weight
 
 
 def calculate_pI(sequence):
     protein_analysis = ProteinAnalysis(sequence)
     pI = protein_analysis.isoelectric_point()
-
     return pI
