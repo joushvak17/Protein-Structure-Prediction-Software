5726b0b24c81dac880d4b3148b11d91a5c146a5f
joushvak17 <kamble.joushva@gmail.com>
2024-04-25 19:28:36 -0400

    Adjusted the label extraction files so that the labels are done for now. I can't add any more labels because there are many limitations that some libraries have as of right now.

--- DataPreparation/LabelExtraction.py
+++ DataPreparation/LabelExtraction.py
@@ -2,8 +2,12 @@ from Bio import PDB
 
 
 def extract_experimental(pdb_file):
-    parser = PDB.PDBParser(QUIET=True)
-    structure = parser.get_structure("protein", pdb_file)
-    experimental = structure.header.get("structure_method", "Unknown")
-    res = structure.header.get("resolution", "Unknown")
-    return experimental, res
+    try:
+        parser = PDB.PDBParser(QUIET=True)
+        structure = parser.get_structure("protein", pdb_file)
+        experimental = structure.header.get("structure_method", "Unknown")
+        res = structure.header.get("resolution", "Unknown")
+
+        return experimental, res
+    except ValueError:
+        return "Unknown", "Unknown"
