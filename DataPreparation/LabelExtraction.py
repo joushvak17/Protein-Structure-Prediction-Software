223d47ba68897ee00c35961c03e216353e13a902
joushvak17 <kamble.joushva@gmail.com>
2024-04-24 20:25:04 -0400

    Adjusted the FeatureExtraction.ipynb and LabelExtraction.py file for R Value and R Free extraction. Added a testing folder that has the LabelExtractionTest.ipynb file that is used to figure out label extraction function.

--- DataPreparation/LabelExtraction.py
+++ DataPreparation/LabelExtraction.py
@@ -2,12 +2,21 @@ from Bio import PDB
 
 
 def extract_experimental(pdb_file):
-    parser = PDB.PDBParser()
-    try:
-        structure = parser.get_structure("protein", pdb_file)
-        experimental = structure.header.get("structure_method", "Unknown")
-        res = structure.header.get("resolution", "Unknown")
-    except Exception as e:
-        print(f"Error parsing {pdb_file}: {str(e)}")
-        experimental, res = "Error", "Unknown"
-    return experimental, res
+    r_value = 50
+    r_free = 50
+    parser = PDB.PDBParser(QUIET=True)
+    structure = parser.get_structure("protein", pdb_file)
+    experimental = structure.header.get("structure_method", "Unknown")
+    res = structure.header.get("resolution", "Unknown")
+
+    with open(pdb_file, "r") as f:
+        for line in f:
+            try:
+                if "REMARK   3   R VALUE" in line:
+                    r_value = float(line.split()[-1])
+                elif "REMARK   3   FREE R VALUE" in line:
+                    r_free = float(line.split()[-1])
+            except ValueError:
+                pass
+
+    return experimental, res, r_value, r_free
