82e53d07bc6713f219197fa33c5288223f215820
Joushva Kamble <kamble.joushva@gmail.com>
2024-08-05 17:53:20 -0400

    Added a retry mechanism to the function that downloads the PDB files

--- DataPreparation/DataCollection.py
+++ DataPreparation/DataCollection.py
@@ -1,6 +1,8 @@
 # Import the needed libraries
 import requests
 import os
+import urllib3
+from retrying import retry
 
 # Define a function that will download a PDB file given a PDB ID
 def download_pdb(pdb_id):
@@ -17,7 +19,16 @@ def download_pdb(pdb_id):
             f.write(response.content)
     else:
         print(f"Failed to download PDB file for {pdb_id}")
-    
+
+# Define a function that will implement a retry strategy up to 5 times
+@retry(stop_max_attempt_number=5, wait_fixed=2000)
+def download_pdb_retry(pdb_id):
+    try:
+        download_pdb(pdb_id)
+    except urllib3.exceptions.HTTPError as e:
+        print(f"Failed to download PDB file for {pdb_id}: {e}")
+        raise
+
 # Get the PDB IDs from the CSV file
 with open("PDBDataID.csv", "r") as f:
     line = f.readline()
@@ -28,6 +39,6 @@ print("The total length of the IDs in the PDBDataID.csv file is: ", len(pdb_ids)
 
 # Download all files from PDB using the IDs that were extracted
 for pdb_id in pdb_ids:
-    download_pdb(pdb_id)
+    download_pdb_retry(pdb_id)
     
 print("The total length of the IDs that were able to be downloaded is: ", len(os.listdir("PDB Data")))
