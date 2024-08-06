0fec6430878513bd2b24d309eacbd39021e6302f
Joushva Kamble <kamble.joushva@gmail.com>
2024-08-06 11:42:34 -0400

    Defined the feature extraction into a script rather than a notebook

---
+++ DataPreparation/FeatureExtraction.py
@@ -0,0 +1,177 @@
+# Import the needed libraries
+import pandas as pd
+
+from Bio import SeqIO, AlignIO
+from Bio.Align import AlignInfo
+from concurrent.futures import ThreadPoolExecutor
+from DataOperations.LabelExtractionOperations import extract_experimental
+from DataOperations.FeatureExtractionOperations import *
+
+def extract_data(seq_record):
+        try:
+            r_value = None
+            r_free = None
+            seq_id = seq_record.id
+            base_pdb_id = seq_id.split("_")[0]
+            pdb_file = f"PDB Data/{base_pdb_id}.pdb"
+            experimental, res = extract_experimental(pdb_file)
+            
+            with open(pdb_file, "r") as f:
+                for line in f:
+                    try:
+                        if "REMARK   3   R VALUE            (WORKING SET) :" in line:
+                            r_value = float(line.split()[-1])
+                        elif "REMARK   3   FREE R VALUE                     :" in line:
+                            r_free  = float(line.split()[-1])
+                    except ValueError:
+                        pass
+                        
+            return seq_id, experimental, res, r_value, r_free
+        except ValueError:
+            return seq_id, None, None, None, None
+
+def main():
+    # Define the unaligned dataframe that will have all the calculated feature values
+    unaligned_data = {"ID": [], 
+                    "Unaligned Sequence": [], 
+                    'A': [], 'R': [], 'N': [], 'D': [],
+                    'C': [], 'E': [], 'Q': [], 'G': [],
+                    'H': [], 'I': [], 'L': [], 'K': [],
+                    'M': [], 'F': [], 'P': [], 'S': [],
+                    'T': [], 'W': [], 'Y': [], 'V': [],
+                    "Hydrophobicity (Kyte-Doolittle Scale)": [],
+                    "Net Charge at pH 7.0 (Neutral)": [],
+                    "Net Charge at pH 3.0 (Acidic)": [],
+                    "Net Charge at pH 11.0 (Basic)": [],
+                    "Isoelectric Point": [],
+                    "Molecular Weight": [],
+                    "Sequence Length": []} 
+
+    for seq_record in SeqIO.parse("FASTAData/Sequences.fasta", "fasta"):
+        
+        unaligned_data["ID"].append(seq_record.id)
+        unaligned_data["Unaligned Sequence"].append(str(seq_record.seq))
+        
+        aa_composition = calculate_a_acid_composition(str(seq_record.seq))
+        for amino_acid, percent in aa_composition.items():
+            unaligned_data[amino_acid].append(percent)
+        
+        hydrophobicity_values = calculate_hydrophobicity(str(seq_record.seq))
+        unaligned_data["Hydrophobicity (Kyte-Doolittle Scale)"].append(hydrophobicity_values)
+        
+        charge_7 = calculate_polarity(str(seq_record.seq), 7.0)
+        unaligned_data["Net Charge at pH 7.0 (Neutral)"].append(charge_7)
+
+        charge_3 = calculate_polarity(str(seq_record.seq), 3.0)
+        unaligned_data["Net Charge at pH 3.0 (Acidic)"].append(charge_3)
+
+        charge_11 = calculate_polarity(str(seq_record.seq), 11.0)
+        unaligned_data["Net Charge at pH 11.0 (Basic)"].append(charge_11)
+        
+        isolectric_values = calculate_pI(str(seq_record.seq))
+        unaligned_data["Isoelectric Point"].append(isolectric_values)
+        
+        mw_values = calculate_mw(str(seq_record.seq))
+        unaligned_data["Molecular Weight"].append(mw_values)
+                
+        sequence_length = len(seq_record.seq)
+        unaligned_data["Sequence Length"].append(sequence_length)    
+
+    # Define the alinged dataframe that will have all the calculated feature values
+
+    # Read the alignment sequences
+    alignment = AlignIO.read("FASTA Data/Aligned_Sequences.fasta", "fasta")
+
+    # Calculate consensus
+    consensus = AlignInfo.SummaryInfo(alignment).dumb_consensus()
+
+    # Calculate Conservation Score
+    start = 0
+    end = alignment.get_alignment_length()
+    e_freq_table = {char: 0.05 for char in "ACDEFGHIKLMNPQRSTVWY"}
+    conservation_score = AlignInfo.SummaryInfo(alignment).information_content(start, end, e_freq_table=e_freq_table, chars_to_ignore=["-"])
+
+    # Initialize variables to store gap statistics
+    alignment_length = end
+    num_sequences = len(alignment)
+    gap_count_per_position = [0] * alignment_length
+
+    # Count the number of gaps at each position
+    for seq_record in alignment:
+        for i, residue in enumerate(str(seq_record.seq)):
+            if residue == "-":
+                gap_count_per_position[i] += 1
+                
+    # Calculate the percentage of gaps at each position
+    perc_gap_per_position = [count / num_sequences * 100 for count in gap_count_per_position]
+
+    # Calculate total number of gaps
+    total_gaps = sum(gap_count_per_position)
+
+    # Calculate average gap length
+    all_gaps = []
+    for seq_record in alignment:
+        sequence = str(seq_record.seq)
+        gaps = [gap for gap in sequence.split('-') if gap]
+        gaps_length = [len(gap) for gap in gaps]
+        all_gaps.extend(gaps_length)
+
+    average_gap_length = sum(all_gaps) / len(all_gaps) if all_gaps else 0
+
+    # Define the aligned dataframe that will have the calculated feature values
+    aligned_data = {"ID": [], 
+                    "Aligned Sequence": [],
+                    "Consensus Sequence": [str(consensus)] * num_sequences, 
+                    "Conservation Scores": [conservation_score] * num_sequences,
+                    "Percentage of Gaps Per Position": [perc_gap_per_position] * num_sequences,
+                    "Total Gaps in Alignment": [total_gaps] * num_sequences,
+                    "Average Gap Length": [average_gap_length] * num_sequences,
+                    "Sequence Length": [],
+                    "Gap Count": [],
+                    "Percentage Gaps": [],
+                    "Mutations from Consensus": []}
+
+    for seq_record in alignment:
+        
+        aligned_data["ID"].append(seq_record.id)
+        aligned_data["Aligned Sequence"].append(str(seq_record.seq))
+        
+        sequence = str(seq_record.seq)
+        len_sequence = len(sequence)
+        gap_count = sequence.count('-')
+        perc_gaps = (gap_count / len_sequence) * 100
+        mutations_from_consensus = sum(c1 != c2 for c1, c2 in zip(sequence, consensus))
+
+        aligned_data["Sequence Length"].append(len_sequence)
+        aligned_data["Gap Count"].append(gap_count)
+        aligned_data["Percentage Gaps"].append(perc_gaps)
+        aligned_data["Mutations from Consensus"].append(mutations_from_consensus)
+
+    # Define the label dataframe that will be the prediction outputs
+    label_data = {"ID": [],
+                "Experimental": [],
+                "Resolution": [],
+                "R Value": [],
+                "R Free": []}
+
+    with (ThreadPoolExecutor() as executor):
+        for seq_id, experimental, res, r_value, r_free in executor.map(extract_data, SeqIO.parse("FASTA Data/Sequences.fasta", "fasta")):
+            label_data["ID"].append(seq_id)
+            label_data["Experimental"].append(experimental)
+            label_data["Resolution"].append(res)
+            label_data["R Value"].append(r_value)
+            label_data["R Free"].append(r_free)
+
+    # Convert the dictionaries to dataframes
+    unaligned_df = pd.DataFrame(unaligned_data)
+    aligned_df = pd.DataFrame(aligned_data)
+    label_df = pd.DataFrame(label_data)
+
+    # Concatenate the dataframes
+    df = pd.concat([unaligned_df, aligned_df, label_df], axis=1)
+
+    # Save the dataframe to a csv file
+    df.to_csv("Data/Features.csv", index=False)
+
+if __name__ == "__main__":
+    main()
