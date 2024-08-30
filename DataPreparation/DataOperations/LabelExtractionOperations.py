98a1687ab16850abbe3cb03fa4e75d69c710a871
Joushva Kamble <kamble.joushva@gmail.com>
2024-08-30 13:26:19 -0400

    Moved the label extractions into one function

--- DataPreparation/DataOperations/LabelExtractionOperations.py
+++ DataPreparation/DataOperations/LabelExtractionOperations.py
@@ -1,13 +1,29 @@
 from Bio import PDB
 
+def extract_data(seq_record):
+        try:
+            r_value = None
+            seq_id = seq_record.id
+            base_pdb_id = seq_id.split("_")[0]
+            pdb_file = f"DataPreparation/PDBData/{base_pdb_id}.pdb"
+            
+            try:
+                parser = PDB.PDBParser(QUIET=True)
+                structure = parser.get_structure("protein", pdb_file)
+                experimental = structure.header.get("structure_method", "Unknown")
+                res = structure.header.get("resolution", "Unknown")
 
-def extract_experimental(pdb_file):
-    try:
-        parser = PDB.PDBParser(QUIET=True)
-        structure = parser.get_structure("protein", pdb_file)
-        experimental = structure.header.get("structure_method", "Unknown")
-        res = structure.header.get("resolution", "Unknown")
-
-        return experimental, res
-    except ValueError:
-        return "Unknown", "Unknown"
+            except ValueError:
+                experimental, res = None, None
+            
+            with open(pdb_file, "r") as f:
+                for line in f:
+                    try:
+                        if "REMARK   3   R VALUE            (WORKING SET) :" in line:
+                            r_value = float(line.split()[-1])
+                    except ValueError:
+                        pass
+                        
+            return experimental, res, r_value
+        except ValueError:
+            return None, None, None
--- DataPreparation/Extractions.py
+++ DataPreparation/Extractions.py
@@ -6,7 +6,7 @@ from Bio import SeqIO, AlignIO
 from Bio.Align import AlignInfo
 from concurrent.futures import ThreadPoolExecutor
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from DataOperations.LabelExtractionOperations import extract_experimental
+from DataOperations.LabelExtractionOperations import extract_data
 from DataOperations.FeatureExtractionOperations import *
 
 def extract_unaligned(path):
@@ -158,35 +158,7 @@ def extract_aligned(path):
         
     return aligned_data
 
-def extract_data(seq_record):
-        try:
-            r_value = None
-            seq_id = seq_record.id
-            base_pdb_id = seq_id.split("_")[0]
-            pdb_file = f"DataPreparation/PDBData/{base_pdb_id}.pdb"
-            experimental, res = extract_experimental(pdb_file)
-            
-            with open(pdb_file, "r") as f:
-                for line in f:
-                    try:
-                        if "REMARK   3   R VALUE            (WORKING SET) :" in line:
-                            r_value = float(line.split()[-1])
-                    except ValueError:
-                        pass
-                        
-            return experimental, res, r_value
-        except ValueError:
-            return None, None, None, None
-
-def main():
-    # Define the path for the unaligned and aligned sequences
-    unaligned_path = "DataPreparation/FASTAData/Sequences.fasta"
-    aligned_path = "DataPreparation/FASTAData/Aligned_Sequences.fasta"
-    
-    # Extract the unaligned and aligned data
-    unaligned_data = extract_unaligned(unaligned_path)
-    aligned_data = extract_aligned(aligned_path)
-
+def extract_labels(path):
     # Define the label dataframe that will be the prediction outputs
     # TODO: Check to see if experimental method is needed
     # - The ID and R Free value have been removed
@@ -195,10 +167,20 @@ def main():
                   "R Value": []}
 
     with (ThreadPoolExecutor() as executor):
-        for experimental, res, r_value in executor.map(extract_data, SeqIO.parse("DataPreparation/FASTAData/Sequences.fasta", "fasta")):
+        for experimental, res, r_value in executor.map(extract_data, SeqIO.parse(path, "fasta")):
             label_data["Experimental"].append(experimental)
             label_data["Resolution"].append(res)
             label_data["R Value"].append(r_value)
+
+def main():
+    # Define the path for the unaligned and aligned sequences
+    unaligned_path = "DataPreparation/FASTAData/Sequences.fasta"
+    aligned_path = "DataPreparation/FASTAData/Aligned_Sequences.fasta"
+    
+    # Extract the unaligned, aligned, and label data
+    unaligned_data = extract_unaligned(unaligned_path)
+    aligned_data = extract_aligned(aligned_path)
+    label_data = extract_labels(unaligned_path)
 
     # Convert the dictionaries to dataframes
     unaligned_df = pd.DataFrame(unaligned_data)
