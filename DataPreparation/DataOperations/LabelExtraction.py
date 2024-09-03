5e71e2e4713226efa2c7d7c2278b3c5549c32512
Joushva Kamble <kamble.joushva@gmail.com>
2024-09-03 18:37:21 -0400

    Added the docstring to the extract label functions

--- DataPreparation/DataOperations/LabelExtraction.py
+++ DataPreparation/DataOperations/LabelExtraction.py
@@ -5,13 +5,20 @@ from DataOperations.LabelExtractionOperations import extract_data
 
 
 def extract_labels(path):
+    """Function to extract the labels from the FASTA file
+
+    Args:
+        path (str): The path to the FASTA file
+    """
     # Define the label dataframe that will be the prediction outputs
     label_data = {"Secondary Structure": [],
                   "Solvent Accessibility": [],
                   "Disorder Regions": []}
     
+    seq_record = SeqIO.read(path, "fasta")
+    
     with (ThreadPoolExecutor() as executor):
-        for secondary_structure, solvent_accessibility, disorder_regions in executor.map(extract_data, SeqIO.parse(path, "fasta")):
+        for secondary_structure, solvent_accessibility, disorder_regions in executor.map(extract_data, seq_record):
             label_data["Secondary Structure"].append(secondary_structure)
             label_data["Solvent Accessibility"].append(solvent_accessibility)
             label_data["Disorder Regions"].append(disorder_regions)
--- DataPreparation/DataOperations/LabelExtractionOperations.py
+++ DataPreparation/DataOperations/LabelExtractionOperations.py
@@ -3,25 +3,33 @@ from Bio import PDB
 
 
 def extract_data(seq_record):
-        try:
-            seq_id = seq_record.id
-            base_pdb_id = seq_id.split("_")[0]
-            pdb_file = f"DataPreparation/PDBData/{base_pdb_id}.pdb"
-            
-            parser = PDB.PDBParser(QUIET=True)
-            structure = parser.get_structure("protein", pdb_file)
-            model = structure[0]
-            dssp = PDB.DSSP(model, pdb_file)
-            
-            # Extract the secondary structure
-            secondary_structure = [dssp[key][2] for key in dssp.keys()]
-            
-            # Extract solvent accessibility
-            solvent_accessibility = [dssp[key][3] for key in dssp.keys()]
+    """Function to extract the labels from the PDB file
 
-            # Extract disorder regions (assuming disorder is marked as 'X' in DSSP)
-            disorder_regions = [1 if dssp[key][2] == 'X' else 0 for key in dssp.keys()]
-                        
-            return secondary_structure, solvent_accessibility, disorder_regions
-        except ValueError:
-            return None, None, None
+    Args:
+        seq_record (str): The sequence record
+
+    Returns:
+        tuple: The secondary structure, solvent accessibility, and disorder regions
+    """
+    try:
+        seq_id = seq_record.id
+        base_pdb_id = seq_id.split("_")[0]
+        pdb_file = f"DataPreparation/PDBData/{base_pdb_id}.pdb"
+        
+        parser = PDB.PDBParser(QUIET=True)
+        structure = parser.get_structure("protein", pdb_file)
+        model = structure[0]
+        dssp = PDB.DSSP(model, pdb_file, dssp="/usr/local/bin/mkdssp")
+        
+        # Extract the secondary structure
+        secondary_structure = [dssp[key][2] for key in dssp.keys()]
+        
+        # Extract solvent accessibility
+        solvent_accessibility = [dssp[key][3] for key in dssp.keys()]
+
+        # Extract disorder regions (assuming disorder is marked as 'X' in DSSP)
+        disorder_regions = [1 if dssp[key][2] == 'X' else 0 for key in dssp.keys()]
+                    
+        return secondary_structure, solvent_accessibility, disorder_regions
+    except ValueError:
+        return None, None, None
