7dcde7c2ad20b4184c85e765af53922e42cc0709
Joushva Kamble <kamble.joushva@gmail.com>
2024-09-12 13:18:16 -0400

    Worked on getting the DSSP values from DSSP files rather than PDB files

--- DataPreparation/DataOperations/LabelExtractionOperations.py
+++ DataPreparation/DataOperations/LabelExtractionOperations.py
@@ -1,4 +1,6 @@
 # Import the needed libraries
+import os
+
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
 
@@ -15,29 +17,34 @@ def extract_data(seq_record):
         seq_id = seq_record.id
         base_pdb_id = seq_id.split("_")[0]
         pdb_file = f"DataPreparation/PDBData/{base_pdb_id}.pdb"
+        dssp_file = f"DataPreparation/DSSPData/{base_pdb_id}.dssp"
         
-        parser = PDBParser(QUIET=True)
-        # FIXME: Check to see if these are the arguments to pass to the parser
-        structure = parser.get_structure(base_pdb_id, pdb_file)
-        model = structure[0]
-        dssp = DSSP(model, pdb_file, dssp="mkdssp")
-        
-        a_key = list(dssp.keys())
-        print(f"DSSP keys for {base_pdb_id}: {a_key}")
-        
-        if not a_key:
-            print(f"No DSSP keys found for {base_pdb_id}, possible issue with PDB or DSSP")
-            return None, None, None
-        
-        # Extract the secondary structure
-        secondary_structure = [dssp[key][2] for key in dssp.keys()]
-        
-        # Extract solvent accessibility
-        solvent_accessibility = [dssp[key][3] for key in dssp.keys()]
+        if os.path.exists(dssp_file): 
+            parser = PDBParser(QUIET=True)
+            structure = parser.get_structure(base_pdb_id, pdb_file)
+            model = structure[0]
+            dssp = DSSP(model, dssp_file, dssp="mkdssp", file_type="DSSP")
+            
+            # TODO: Remove these check statements and add values to the tuple
+            a_key = list(dssp.keys())
+            print(f"DSSP keys for {base_pdb_id}: {a_key}")
+            
+            if not a_key:
+                print(f"No DSSP keys found for {base_pdb_id}, possible issue with PDB or DSSP")
+                return None, None, None
+            
+            # Extract the secondary structure
+            secondary_structure = [dssp[key][2] for key in dssp.keys()]
+            
+            # Extract solvent accessibility
+            solvent_accessibility = [dssp[key][3] for key in dssp.keys()]
 
-        # Extract disorder regions (assuming disorder is marked as 'X' in DSSP)
-        disorder_regions = [1 if dssp[key][2] == 'X' else 0 for key in dssp.keys()]
-                    
-        return secondary_structure, solvent_accessibility, disorder_regions
+            # Extract disorder regions (assuming disorder is marked as 'X' in DSSP)
+            disorder_regions = [1 if dssp[key][2] == 'X' else 0 for key in dssp.keys()]
+                        
+            return secondary_structure, solvent_accessibility, disorder_regions
+        else:
+            print(f"DSSP file {dssp_file} does not exist")
+            return None, None, None
     except ValueError:
         return None, None, None
