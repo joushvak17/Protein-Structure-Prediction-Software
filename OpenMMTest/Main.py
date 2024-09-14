d084799004dcc008191d42f4f5797cc98b6d2cfe
Joushva Kamble <kamble.joushva@gmail.com>
2024-09-14 13:54:26 -0400

    Added the system creation from the forcefiled, will need to fix the PDB files

--- OpenMMTest/Main.py
+++ OpenMMTest/Main.py
@@ -5,6 +5,7 @@ import random
 
 from openmm import *
 from openmm import app
+from openmm import unit
 
 def main():
     # Load a PDB file from the DataPreparation/PDBData folder
@@ -19,17 +20,20 @@ def main():
     # Load the PDB file
     pdb = app.PDBFile(pdb_file) 
     
-    # Ask the user what type of force field they want to use
-    force_field = input("Which force field would you like to use? (amber14-all.xml, charmm36.xml, or oplsaa.xml): ")
+    # NOTE: For now go with a set force field. Ask the user what type of force field they want to use
+    # force_field = input("Which force field would you like to use? (amber14-all.xml, charmm36.xml, or oplsaa.xml): ")
+    force_field = 'amber14-all.xml'
 
-    # Ask the user what type of water model they want to use
-    water_model = input("Which water model would you like to use? (tip3pfb.xml, tip4pew.xml, or spce.xml): ")
+    # NOTE: For no go with a set water model. Ask the user what type of water model they want to use
+    # water_model = input("Which water model would you like to use? (tip3pfb.xml, tip4pew.xml, or spce.xml): ")
+    water_model = 'amber14/tip3pfb.xml'
     
     # Create a force field, first file is the force field, second file is the water model
     forcefield = app.ForceField(force_field, water_model)
 
+    # TODO: Will need to fix the PDB files first
     # Create the system
-    system = forcefield.createSystem(pdb.topology, nonbondedCutoff=1.0*nanometers)
+    system = forcefield.createSystem(topology=pdb.topology, nonbondedMethod=app.PME, nonbondedCutoff=unit.Quantity(value=1.0, unit=unit.nano*unit.meters))
 
     # Set up the integrator
     integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
